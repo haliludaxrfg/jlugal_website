52a1593c1cf4597eaf8176faff2efc6c5f13d49b
Zhen Lan <2726165926@qq.com>
2026-09-14 10:16:33 +0800

    beta 0.8.1 增减图片与触屏适配

--- gallery-data.js
+++ gallery-data.js
@@ -1,14 +1,17 @@
 window.galleryData = {
     gallery1: {
         title: '旮旯直播',
-        files: ['12J器.png', '4rb.jpeg', 'fpssw.jpeg', 'new1.png', 'new2.jpeg', '神人1.jpeg', '神人2.jpeg', '神人3.jpeg']
+        files: ['12J器.png', '4rb.jpeg', 'fpssw.jpeg','new1.jpg','new2.jpeg', '神人1.jpeg', '神人2.jpeg', '神人3.jpeg']
     },
     gallery2: {
         title: '线虫聚会',
-        files: ['聚餐1.png']
+        files: ['聚餐1.png','妃爱蛋糕.jpg']
     },
     gallery3: {
         title: '神必南梁',
-        files: ['empty_set1.jpeg', 'empty_set2.jpeg', 'empty_set3.jpeg', 'if1.jpeg', 'if2.jpeg', 'jdyjs1.jpeg', 'lpluma1.jpeg', 'lpluma2.jpeg', 'M1.jpeg', 'yny1.jpeg', 'ysf1.jpeg', 'yyz1.jpeg', 'yyz2.jpeg', 'yyz3.jpeg', 'yyz4.jpeg']
+        files: ['xdbyx.jpg','lp_yyz.jpg','empty_set1.jpeg', 'empty_set2.jpeg', 
+                'empty_set3.jpeg', 'if1.jpeg', 'if2.jpeg', 'jdyjs1.jpeg', 'lpluma1.jpeg', 
+                'lpluma2.jpeg', 'M1.jpeg', 'yny1.jpeg', 'ysf1.jpeg', 
+                'yyz1.jpeg', 'yyz2.jpeg', 'yyz3.jpeg', 'yyz4.jpeg']
     }
 };
